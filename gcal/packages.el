941878692efa6560fcb72b42c0b65c3b8f1627c0
p-baleine <p.baleine@gmail.com>
2018-10-17 15:18:50 +0900

    Add gcal layer.

---
+++ gcal/packages.el
@@ -0,0 +1,67 @@
+;;; packages.el --- gcal layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: 田嶋隼平 <junpei-tajima@tajimashuntairanoMacBook-ea.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `gcal-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `gcal/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `gcal/pre-init-PACKAGE' and/or
+;;   `gcal/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst gcal-packages
+  '(org-gcal)
+  "The list of Lisp packages required by the gcal layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+(defun gcal/init-org-gcal ()
+  (setq
+   org-gcal-client-id gcal-org-gcal-client-id
+   org-gcal-client-secret gcal-org-gcal-client-secret
+   org-gcal-file-alist gcal-org-gcal-file-alist))
+
+;;; packages.el ends here
